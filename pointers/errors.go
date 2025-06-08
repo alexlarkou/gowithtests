6770f3beb761c91831224f373b7e854c8cecadd9
Alex Larkou <alex.larkou@live.com>
2025-06-08 18:05:41 +0200

    add pointers

---
+++ pointers/errors.go
@@ -0,0 +1,5 @@
+package pointers
+
+import "errors"
+
+var ErrInsufficientFunds = errors.New("cannot withdraw, insufficient funds")
---
+++ pointers/wallet.go
@@ -0,0 +1,23 @@
+package pointers
+
+type Wallet struct {
+	balance Bitcoin
+}
+
+func (w *Wallet) Deposit(amount Bitcoin) {
+	w.balance += amount
+}
+
+func (w *Wallet) Withdraw(amount Bitcoin) error {
+
+	if amount > w.balance {
+		return ErrInsufficientFunds
+	}
+
+	w.balance -= amount
+	return nil
+}
+
+func (w *Wallet) Balance() Bitcoin {
+	return w.balance
+}
